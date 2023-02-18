ed256c7ad6304319df1cf8e411c31de5e98a6398
adcavalcant <aderc19@gmail.com>
2023-02-18 15:32:08 -0300

    Criação do Service da aplicação

---
+++ frontend/src/app/components/pensamentos/pensamento.service.ts
@@ -0,0 +1,8 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({ // Decorator responsável por sinalizar pro Angular que essa classe é injetável.
+  providedIn: 'root', // provedor que diz que esse decorator é visível em toda a aplicação
+})
+export class PensamentoService {
+  constructor() {}
+}
