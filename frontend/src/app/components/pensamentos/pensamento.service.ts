c236cd88136d32b52501dc3bcf9b985f33b4170c
adcavalcant <aderc19@gmail.com>
2023-02-18 15:37:49 -0300

    Adicionando injeção da dependência http no Service criado

--- frontend/src/app/components/pensamentos/pensamento.service.ts
+++ frontend/src/app/components/pensamentos/pensamento.service.ts
@@ -1,8 +1,12 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
-@Injectable({ // Decorator responsável por sinalizar pro Angular que essa classe é injetável.
-  providedIn: 'root', // provedor que diz que esse decorator é visível em toda a aplicação
+@Injectable({
+  // Decorator responsável por sinalizar pro Angular que essa classe é injetável.
+  providedIn: 'root', // provedor que diz que esse decorator é visível em toda a aplicação+
 })
 export class PensamentoService {
-  constructor() {}
+  constructor(private http: HttpClient) {}
+
+
 }
