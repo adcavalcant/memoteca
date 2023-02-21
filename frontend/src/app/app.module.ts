6728fa4051da9952e5e8bfc6ac4ad4459274950d
adcavalcant <aderc19@gmail.com>
2023-02-21 11:50:19 -0300

    ajustes

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,10 +4,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+
+//componente card de pensamento
+import { PensamentoComponent } from './componentes/pensamentos/pensamento/pensamento.component';
+
+//componentes de cabeçalho e rodapé
 import { CabecalhoComponent } from './componentes/cabecalho/cabecalho.component';
 import { RodapeComponent } from './componentes/rodape/rodape.component';
-import { FormsModule } from '@angular/forms';
-import { PensamentoComponent } from './componentes/pensamentos/pensamento/pensamento.component';
+
+//componentes do crud
 import { ListarPensamentoComponent } from '../app/componentes/pensamentos/listar-pensamento/listar-pensamento.component';
 import { CriarPensamentoComponent } from '../app/componentes/pensamentos/criar-pensamento/criar-pensamento.component';
 import { ExcluirPensamentoComponent } from '../app/componentes/pensamentos/excluir-pensamento/excluir-pensamento.component';
